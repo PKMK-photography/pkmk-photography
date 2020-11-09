42a801b1a04919d3427d4381e346257f0796eec7
mattcbodily <mattcbodily@gmail.com>
2020-11-09 15:11:12 -0700

    boiler-plate setup

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const Header = props => {
+    let [dropdown, setDropdown] = useState(false);
+
+    return (
+        <header>
+
+        </header>
+    )
+}
+
+export default Header;
