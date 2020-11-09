bc1c74be6e74e5bbacf0a67831b77f252dc1715a
mattcbodily <mattcbodily@gmail.com>
2020-11-09 15:31:05 -0700

    built mobile version of Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
+import menuIcon from '../../assets/icons/menu.svg';
+import './Header.css';
 
 const Header = props => {
     let [dropdown, setDropdown] = useState(false);
 
     return (
-        <header>
-
+        <header className='main-header'>
+            <h1>P K M K // photography</h1>
+            <img src={menuIcon} alt='Main menu'/>
         </header>
     )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = props => (
     <main>
-        
+        Home
     </main>
 )
 
