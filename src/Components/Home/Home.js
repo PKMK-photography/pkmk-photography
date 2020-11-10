730d2b84c8b0856169f6f69a5f393f77fbaf92aa
mattcbodily <mattcbodily@gmail.com>
2020-11-10 11:20:13 -0700

    about-us section initial layout complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,7 @@ import './Home.css';
 const Home = props => (
     <main className='home'>
         <div className='home-image'/>
-        <section className='about-us-section'>
+        <section className='intro-section'>
             <h1>Why We Chose Photography</h1>
             <p className='intro-blurb'>For us, photography is our way of life. We thrive when we have a camera in our hands and we live to help capture the moments that matter most to you. We consider ourselves to be "Lifestyle Photographers" which means we want to capture pictures of you that are genuine and true to your character. Whether it's taking your family photo session or photographing your wedding, we promise to provide you with the pictures through which you can relive your happiest memories.</p>
             <h5 className='book-with-us'>BOOK WITH US!</h5>
@@ -50,6 +50,17 @@ const Home = props => (
                 <button className='portfolio-button'>PORTFOLIO</button>
             </section>
         </section>
+        <section className='about-us-section'>
+            <h1>About Us</h1>
+            <h5>WE ARE PRESTON AND MCKALIE KAHLE</h5>
+            <p>We've each had a passion for photography since we were young and being able to work together as a married couple has helped us to hone our skills and develop our craft even more.</p>
+            <p>"For me, it's all about getting that one special shot - the money shot. The one that makes you stop and let your mind drift back to that moment no matter how long ago it was."</p>
+            <p>-Preston</p>
+            <p>"I love capturing raw moments. I feel like photography allows me to see people for who they really are and I love seeing all the elements come together to make a great photo. When people look at my photography I want them to <span>feel</span> something."</p>
+            <p>-McKalie</p>
+            <div className='box-underline'/>
+        </section>
+        <div className='ending-image'/>
     </main>
 )
 
