f19d829e777ee26299f833b5f64754873d6baaf6
mattcbodily <mattcbodily@gmail.com>
2020-11-10 14:14:33 -0700

    initial layout and functionality of the main menu is complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import menuIcon from '../../assets/icons/menu.svg';
+import closeIcon from '../../assets/icons/x.svg';
 import './Header.css';
 
 const Header = props => {
@@ -8,11 +10,15 @@ const Header = props => {
     return (
         <header className='main-header'>
             <h1>P K M K // photography</h1>
-            <img src={menuIcon} alt='Main menu'/>
+            <img src={menuIcon} alt='Open menu' onClick={() => setMainMenu(true)}/>
             {mainMenu
                 ? (
                     <nav className='main-menu'>
-                        
+                        <img src={closeIcon} alt='Close menu' className='close-icon' onClick={() => setMainMenu(false)}/>
+                        <Link to='/' className='main-links' onClick={() => setMainMenu(false)}>HOME</Link>
+                        <Link to='/services' className='main-links' onClick={() => setMainMenu(false)}>SESSIONS</Link>
+                        <Link to='/project-gallery' className='main-links' onClick={() => setMainMenu(false)}>PORTFOLIO</Link>
+                        <Link to='/contact' className='main-links' onClick={() => setMainMenu(false)}>CONTACT</Link>
                     </nav>
                 )
                 : null}
