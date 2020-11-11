2152f4616e87feaa99ab9ca309cd94b036824b9d
mattcbodily <mattcbodily@gmail.com>
2020-11-11 13:35:23 -0700

    changed main home image, added responsive styling to the intro section of the Home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,11 +14,15 @@ const Home = props => {
         <main className='home'>
             <div className='home-image'/>
             <section className='intro-section'>
-                <h1>Why We Chose Photography</h1>
-                <p className='intro-blurb'>For us, photography is our way of life. We thrive when we have a camera in our hands and we live to help capture the moments that matter most to you. We consider ourselves to be "Lifestyle Photographers" which means we want to capture pictures of you that are genuine and true to your character. Whether it's taking your family photo session or photographing your wedding, we promise to provide you with the pictures through which you can relive your happiest memories.</p>
-                <h5 className='book-with-us'>BOOK WITH US!</h5>
-                <p>Take a look at the types of sessions we offer and find the date that works for you.</p>
-                <button>CHEESE!</button>
+                <section className='intro-section-flex-one'>
+                    <h1>Why We Chose Photography</h1>
+                    <p className='intro-blurb'>For us, photography is our way of life. We thrive when we have a camera in our hands and we live to help capture the moments that matter most to you. We consider ourselves to be "Lifestyle Photographers" which means we want to capture pictures of you that are genuine and true to your character. Whether it's taking your family photo session or photographing your wedding, we promise to provide you with the pictures through which you can relive your happiest memories.</p>
+                </section>
+                <section className='intro-section-flex-two'>
+                    <h5 className='book-with-us'>BOOK WITH US!</h5>
+                    <p>Take a look at the types of sessions we offer and find the date that works for you.</p>
+                    <button>CHEESE!</button>
+                </section>
             </section>
             <div className='our-work-image'/>
             <section className='our-work-section'>
