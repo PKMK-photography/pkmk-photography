a56ac547b2c35ee4ad0769eca4507bcc8c71f6ec
mattcbodily <mattcbodily@gmail.com>
2020-11-11 10:43:13 -0700

    tablet view for the Header is complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,18 +10,28 @@ const Header = props => {
     return (
         <header className='main-header'>
             <h1>P K M K // photography</h1>
-            <img src={menuIcon} alt='Open menu' onClick={() => setMainMenu('open')}/>
-            {mainMenu === 'open'
-                ? (
-                    <nav className={`main-menu ${mainMenu}`}>
-                        <img src={closeIcon} alt='Close menu' className='close-icon' onClick={() => setMainMenu('closed')}/>
-                        <Link to='/' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>HOME</Link>
-                        <Link to='/services' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>SESSIONS</Link>
-                        <Link to='/project-gallery' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>PORTFOLIO</Link>
-                        <Link to='/contact' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>CONTACT</Link>
-                    </nav>
-                )
-                : null}
+            <img src={menuIcon} alt='Open menu' className='mobile' onClick={() => setMainMenu('open')}/>
+            <nav className={`main-menu ${mainMenu}`}>
+                <img src={closeIcon} alt='Close menu' className='close-icon' onClick={() => setMainMenu('closed')}/>
+                <Link to='/' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>HOME</Link>
+                <Link to='/services' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>SESSIONS</Link>
+                <Link to='/project-gallery' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>PORTFOLIO</Link>
+                <Link to='/contact' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>CONTACT</Link>
+            </nav>
+            <nav className='desktop-menu'>
+                <Link to='/'>
+                    <button>HOME</button>
+                </Link>
+                <Link to='/services'>
+                    <button>SESSIONS</button>
+                </Link>
+                <Link to='/project-gallery'>
+                    <button>PORTFOLIO</button>
+                </Link>
+                <Link to='/contact'>
+                    <button>CONTACT</button>
+                </Link>
+            </nav>
         </header>
     )
 }
