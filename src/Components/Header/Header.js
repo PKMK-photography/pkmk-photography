effed611e8c9c53224e12a108fca86d65424fd10
mattcbodily <mattcbodily@gmail.com>
2020-11-11 09:29:00 -0700

    added opening animation to the main menu in Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,20 +5,20 @@ import closeIcon from '../../assets/icons/x.svg';
 import './Header.css';
 
 const Header = props => {
-    let [mainMenu, setMainMenu] = useState(false);
+    let [mainMenu, setMainMenu] = useState('closed');
 
     return (
         <header className='main-header'>
             <h1>P K M K // photography</h1>
-            <img src={menuIcon} alt='Open menu' onClick={() => setMainMenu(true)}/>
-            {mainMenu
+            <img src={menuIcon} alt='Open menu' onClick={() => setMainMenu('open')}/>
+            {mainMenu === 'open'
                 ? (
-                    <nav className='main-menu'>
-                        <img src={closeIcon} alt='Close menu' className='close-icon' onClick={() => setMainMenu(false)}/>
-                        <Link to='/' className='main-links' onClick={() => setMainMenu(false)}>HOME</Link>
-                        <Link to='/services' className='main-links' onClick={() => setMainMenu(false)}>SESSIONS</Link>
-                        <Link to='/project-gallery' className='main-links' onClick={() => setMainMenu(false)}>PORTFOLIO</Link>
-                        <Link to='/contact' className='main-links' onClick={() => setMainMenu(false)}>CONTACT</Link>
+                    <nav className={`main-menu ${mainMenu}`}>
+                        <img src={closeIcon} alt='Close menu' className='close-icon' onClick={() => setMainMenu('closed')}/>
+                        <Link to='/' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>HOME</Link>
+                        <Link to='/services' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>SESSIONS</Link>
+                        <Link to='/project-gallery' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>PORTFOLIO</Link>
+                        <Link to='/contact' className={`main-links links-${mainMenu}`} onClick={() => setMainMenu('closed')}>CONTACT</Link>
                     </nav>
                 )
                 : null}
