0fb1e618104715b9ace8ff6ed574d2b0dc260652
mattcbodily <mattcbodily@gmail.com>
2020-12-18 11:02:30 -0700

    adding code-splitting to boost performance

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Components/Home/Home';
-import Sessions from './Components/Sessions/Sessions';
-import Portfolio from './Components/Portfolio/Portfolio';
-import Contact from './Components/Contact/Contact';
+const Sessions = lazy(() => import('./Components/Sessions/Sessions'));
+const Portfolio = lazy(() => import('./Components/Portfolio/Portfolio'));
+const Contact = lazy(() => import('./Components/Contact/Contact'));
 
 export default (
-    <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/services' component={Sessions}/>
-        <Route path='/project-gallery' component={Portfolio}/>
-        <Route path='/contact' component={Contact}/>
-    </Switch>
+    <Suspense fallback={<div id='loading-container'>Loading...</div>}>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/services' component={Sessions}/>
+            <Route path='/project-gallery' component={Portfolio}/>
+            <Route path='/contact' component={Contact}/>
+        </Switch>
+    </Suspense>
 );
