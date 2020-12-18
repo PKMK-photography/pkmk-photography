a8aab70c39eed86682a3e794ba52f966bbbcf910
mattcbodily <mattcbodily@gmail.com>
2020-12-18 11:23:24 -0700

    added loading animation

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import React, {Suspense, lazy} from 'react';
 import {Switch, Route} from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 import Home from './Components/Home/Home';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 const Sessions = lazy(() => import('./Components/Sessions/Sessions'));
 const Portfolio = lazy(() => import('./Components/Portfolio/Portfolio'));
 const Contact = lazy(() => import('./Components/Contact/Contact'));
 
 export default (
-    <Suspense fallback={<div id='loading-container'>Loading...</div>}>
+    <Suspense 
+        fallback={<div id='loading-container'>
+                    <Loader 
+                        type='ThreeDots'
+                        color='#646464'
+                        height={100}
+                        width={100}/>
+                  </div>}>
         <Switch>
             <Route exact path='/' component={Home}/>
             <Route path='/services' component={Sessions}/>
